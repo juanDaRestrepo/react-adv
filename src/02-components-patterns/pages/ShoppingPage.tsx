e163a274bcea978d6ff02c11fefc3b33fea27a9c
Daniel Restrepo <jdrestrepo@unitecnica.net>
2024-02-07 21:43:43 -0500

    Finish custom component

--- src/02-components-patterns/pages/ShoppingPage.tsx
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -1,12 +1,25 @@
 import { ProductCard } from "../components/ProductCard"
 
+const product = {
+  id: '1',
+  title: 'Coffee Mug -Card',
+  img: './coffee-mug.png'
+}
 
 export const ShoppingPage = () => {
   return (
     <div>
         <h1>ShoppingPage</h1>
         <hr />
-        <ProductCard />
+
+        <div style={{
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap'
+        }}>
+          <ProductCard product={product}/>
+        </div>
+        
     </div>
   )
 }
